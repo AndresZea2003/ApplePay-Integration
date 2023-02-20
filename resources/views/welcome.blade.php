f98c1db5f65f1c13184c53d3bffad894018df0a1
Andres Felipe Zea Acevedo <andres.zea@evertecinc.com>
2023-02-20 12:12:49 -0500

    feat: Vue.js installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-<div class="bg-violet-500">Tailwind Instalado</div>
+@extends('layouts.app')
 
-</body>
-</html>
+@section('main')
+
+    <div class="bg-blue-200">Tailwind Instalado</div>
+    <component-vue></component-vue>
+
+@endsection
